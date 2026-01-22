8655ca97cc74172fb8f96fbcf7fc76b05cae4597
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-01-22 09:20:24 +0200

    [169] [Time Beats: 20.12%] [Memory Beats: 96.31%] - LeetPush

--- Majority-Element.cs
+++ Majority-Element.cs
@@ -3,36 +3,27 @@
 3public class Solution {
 4    public int MajorityElement(int[] nums) {
 5        Dictionary<int, int> elementCounts = new();
-6        
-7        
-8        foreach (int num in nums)
-9        {
-10           
-11            ref int count = ref System.Runtime.InteropServices.CollectionsMarshal
-12                .GetValueRefOrAddDefault(elementCounts, num, out _);
-13            
-14            count++;
-15        }
-16        
-17        int majorityCount = nums.Length / 2;
-18
-19     
-20        
-21        int counter = 0;
-22        int maxNumber = int.MinValue;
-23        foreach (var kvp in elementCounts)
-24        {
-25            if (kvp.Value > majorityCount)
-26            {
-27                maxNumber = Math.Max(maxNumber, kvp.Key);
-28                counter++;
-29            }
-30            
-31         
-32            if (counter == 2)
-33                break;
-34        }
-35        
-36        return maxNumber;
-37    }
-38}
+6        int majorityCount = nums.Length / 2;
+7        int counter = 0;
+8        int maxNumber = int.MinValue;
+9        foreach (int num in nums)
+10        {
+11           
+12            ref int count = ref System.Runtime.InteropServices.CollectionsMarshal
+13                .GetValueRefOrAddDefault(elementCounts, num, out _);
+14            
+15            count++;
+16
+17            if (elementCounts[num] > majorityCount)
+18            {
+19                maxNumber = Math.Max(maxNumber, num);
+20                counter++;
+21            }
+22            
+23         
+24            if (counter == 2)
+25                break;
+26        }
+27        return maxNumber;
+28    }
+29}
