5b52e4cf3df708c507e89b77ceee59e62449cb28
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-02-02 17:00:03 +0200

    [347] [Time Beats: 75.72%] [Memory Beats: 39.73%] - LeetPush

--- Top-K-Frequent-Elements.cs
+++ Top-K-Frequent-Elements.cs
@@ -1,20 +1,41 @@
-1public class Solution {
-2    public int[] TopKFrequent(int[] nums, int k) 
-3    {
-4        var frequencyMap = new Dictionary<int,int>();
-5        foreach (var num in nums)
-6        {
-7            ref var count = ref System.Runtime.InteropServices.CollectionsMarshal.GetValueRefOrAddDefault(frequencyMap, num, out bool exists);
-8           if (exists)
-9               count++;
-10           else
-11                count = 1;
-12        }  
-13
-14        return frequencyMap
-15            .OrderByDescending(kv => kv.Value)
-16            .Take(k)
-17            .Select(kv => kv.Key)
-18            .ToArray();  
-19    }
-20}
+1public class Solution
+2{
+3    public int[] TopKFrequent(int[] nums, int k)
+4    {
+5        var frequencyMap = new Dictionary<int, int>();
+6        foreach (var num in nums)
+7        {
+8            ref var count = ref System.Runtime.InteropServices.CollectionsMarshal.GetValueRefOrAddDefault(frequencyMap, num, out bool exists);
+9            if (exists)
+10                count++;
+11            else
+12                count = 1;
+13        }
+14
+15        List<List<int>> bucket= new List<List<int>>(new List<int>[nums.Length + 1]);
+16        int endIndex = 0;
+17        foreach (var kvp in frequencyMap)
+18        {
+19            int freq = kvp.Value;
+20            if(bucket[freq] == null)
+21                bucket[freq] = new List<int>();
+22            bucket[freq].Add(kvp.Key);
+23            endIndex = Math.Max(endIndex, freq);
+24        }
+25        List<int> result = new List<int>();
+26        for (int i = endIndex; i >= 0 && result.Count < k; i--)
+27        {
+28            if (bucket[i] != null)
+29            {
+30                foreach (var num in bucket[i])
+31                {
+32                    result.Add(num);
+33                    if (result.Count == k)
+34                        break;
+35                }
+36            }
+37        }
+38        return result.ToArray();
+39
+40    }
+41}
