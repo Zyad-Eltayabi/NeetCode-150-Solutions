108f008c825a96629b1b92b4c1eaf8a48a918ea6
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-09-03 10:48:17 +0300

    [121] [Time Beats: 99.83%] [Memory Beats: 31.42%] - LeetPush

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (int price in prices)
+        {
+            if (price < minPrice)
+            {
+                minPrice = price;
+            }
+            else
+            {
+                int profit = price - minPrice;
+
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
